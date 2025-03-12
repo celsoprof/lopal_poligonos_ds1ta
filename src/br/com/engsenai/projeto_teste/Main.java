d467e8437ef92f9f796b4fdb79e0009024d6b86c
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2025-03-12 16:46:28 -0300

    Classe Quadrado concluída.

---
+++ src/br/com/engsenai/projeto_teste/Main.java
@@ -0,0 +1,15 @@
+package br.com.engsenai.projeto_teste;
+
+import br.com.engsenai.projeto_teste.model.Quadrado;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Quadrado q1 = new Quadrado();
+		q1.setLado(-5);
+		q1.mostrarDados();
+
+	}
+
+}
---
+++ src/br/com/engsenai/projeto_teste/banco/Acesso.java
@@ -0,0 +1,13 @@
+package br.com.engsenai.projeto_teste.banco;
+
+import br.com.engsenai.projeto_teste.model.Quadrado;
+
+public class Acesso {
+	
+	private void gravar() {
+		Quadrado q2 = new Quadrado();
+		q2.setLado(-8);
+		q2.mostrarDados();
+	}
+
+}
