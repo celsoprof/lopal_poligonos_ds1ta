e2dbb620920abcfe492a9d4e12309f7ac2e31417
Prof Celso <celso.furtado@sp.senai.br>
2025-03-19 17:02:41 -0300

    Classes Quadrado e Trapézio concluídos

--- src/br/com/engsenai/projeto_teste/model/Quadrado.java
+++ src/br/com/engsenai/projeto_teste/model/Quadrado.java
@@ -1,39 +1,32 @@
 package br.com.engsenai.projeto_teste.model;
 
 public class Quadrado {
-
+	
 	private double lado;
 	
-	public void setLado(double lado) {
-		if (lado < 0) {
-			this.lado = 3;
-		} else {
-			this.lado = lado;
-		}
-		
-	}
-	
-	private double calcularArea() {
-		double area = Math.pow(lado, 2);
-		return area;
-	}
-	
-	private double calcularPerimetro() {
-		double perimetro = 4 * lado;
-		return perimetro;
-	}
-	
-	public void mostrarDados() {
-		System.out.println("------------------");
-		System.out.println("DADOS DO QUADRADO");
-		System.out.println("------------------");
-		System.out.println("LADO:      " + lado);
-		System.out.println("ÁREA:      " + calcularArea());
-		System.out.println("PERÍMETRO: " + calcularPerimetro());
-		System.out.println("------------------");
-	}
-	
+	// Método para definir o valor do lado
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    private double calcularArea() {
+        return lado * lado;
+    }
+
+    private double calcularPerimetro() {
+        return 4 * lado;
+    }
+
+    public void mostrarDados() {
+    	System.out.println("-----------------------------------");
+    	System.out.println("Q U A D R A D O");
+    	System.out.println("-----------------------------------");
+        System.out.println("Lado: " + lado);
+        System.out.println("Área: " + calcularArea());
+        System.out.println("Perímetro: " + calcularPerimetro());
+        System.out.println("-----------------------------------");
+    }
+
+    
+
 }
-
-
-
