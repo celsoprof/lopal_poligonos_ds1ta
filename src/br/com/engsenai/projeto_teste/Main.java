7f6bc121780da6e5a67af877c014009cd82cb1f2
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2025-03-26 17:02:57 -0300

    Implementação Scanner

--- src/br/com/engsenai/projeto_teste/Main.java
+++ src/br/com/engsenai/projeto_teste/Main.java
@@ -1,11 +1,16 @@
 package br.com.engsenai.projeto_teste;
 
+import java.util.Scanner;
+
+import br.com.engsenai.projeto_teste.model.Quadrado;
+import br.com.engsenai.projeto_teste.ui.Menu;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		
-
+		Menu menu = new Menu();
+		menu.criarMenu();
 	}
 
 }
---
+++ src/br/com/engsenai/projeto_teste/ui/Menu.java
@@ -0,0 +1,27 @@
+package br.com.engsenai.projeto_teste.ui;
+
+import java.util.Scanner;
+
+public class Menu {
+	
+	public void criarMenu() {
+		System.out.println("-----------------------------");
+		System.out.println("CALCULADORA DE POLÍGONOS");
+		System.out.println("-----------------------------");
+		System.out.println("1 - Quadrado");
+		System.out.println("2 - Triângulo");
+		System.out.println("3 - Trapézio");
+		System.out.println("4 - Retângulo");
+		System.out.println("5 - Circunferência");
+		System.out.println("6 - Sair");
+		System.out.println("----------------------------");
+		System.out.print("Escolha uma opção (1 - 6): ");
+		
+		Scanner leitor = new Scanner(System.in);
+		int opcao = leitor.nextInt();
+		
+		System.out.println("A opção selecionada foi " + opcao);
+		
+	}
+
+}
